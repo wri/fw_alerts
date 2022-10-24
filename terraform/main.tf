76c19f55f6adc3e2f2cce2711d50c0f981c5fe6e
Edward Martin <edward@3sidedcube.com>
2022-10-24 18:07:27 +0100

    tf path-pattern

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ module "fargate_autoscaling" {
   lb_target_group_arn = module.fargate_autoscaling.lb_target_group_arn
   listener_arn = data.terraform_remote_state.fw_core.outputs.lb_listener_arn
   project_prefix = var.project_prefix
-  path_pattern = ["/v1/fw-alerts*","/v1/fw_alerts/healthcheck"]
+  path_pattern = ["/v1/fw-alerts*","/v1/fw_alerts/healthcheck","/v3/alerts*"]
   health_check_path = "/v1/fw_alerts/healthcheck"
   priority = 2
 }
